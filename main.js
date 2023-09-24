645106d4dc9ef0db7d868204e273916e85a8b805
JUMTBOX <yjey12@naver.com>
2023-09-24 19:16:45 +0900

    ...

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 const { app, BrowserWindow, ipcMain } = require("electron");
 const path = require("path");
 const axios = require("axios");
-const { Builder, By, Key, until } = require("selenium-webdriver");
-const chrome = require("selenium-webdriver/chrome");
 const num2kr = require("./modules/num2kr");
 const hardCoding = require("./modules/hardcode");
 
@@ -36,6 +34,7 @@ app.on("window-all-closed", () => {
     app.quit();
   }
 });
+
 //icp...
 /////////////////////////////////////////////////////////
 const ahaFunc = async (word) => {
@@ -61,57 +60,6 @@ const ahaFunc = async (word) => {
     }
   }
   return result;
-};
-////////////////////////////////////////////////////////
-const papagoFunc = async (text) => {
-  let driver = await new Builder()
-    .forBrowser("chrome")
-    .setChromeOptions(new chrome.Options().headless())
-    .build();
-
-  try {
-    //파파고 접속하기
-    await driver.get("https://papago.naver.com/");
-
-    //언어 선택 드롭다운 버튼
-    let dropBtn = await driver.findElement(By.css("#ddSourceLanguageButton"));
-
-    await driver
-      .wait(until.elementLocated(By.css("#ddSourceLanguageButton")), 3000)
-      .then(dropBtn.click());
-
-    //언어 목록 중 영어를 선택
-    let enBtn = await driver.findElement(
-      By.css(
-        "#ddSourceLanguage > div.dropdown_menu___XsI_h.active___3VPGL > ul > li:nth-child(3)"
-      )
-    );
-    let action = driver.actions({ async: true });
-    await action.move({ origin: enBtn }).click().perform();
-
-    //단어 입력란 찾아가서 인자로 받은 text 입력
-    let textInput = await driver.findElement(By.css("#txtSource"));
-
-    await textInput.sendKeys(text, Key.ENTER);
-
-    //단어 입력한 후 한글 발음이 표시되는 span태그가 나타나길 기다림
-    await driver.wait(
-      until.elementLocated(By.css(".diction_text___1alha span")),
-      10000
-    );
-    let resultEl = await driver.findElement(
-      By.css(".diction_text___1alha span")
-    );
-    // 한글 발음이 표시된 span 태그가 나오면 태그안의 텍스트를 가져옴
-    let result = await resultEl.getText();
-    return result;
-  } catch (err) {
-    return "fail";
-  } finally {
-    setTimeout(() => {
-      driver.quit();
-    }, 1000);
-  }
 };
 
 ipcMain.handle("fetch", async (evt, text) => {
@@ -141,22 +89,15 @@ ipcMain.handle("fetch", async (evt, text) => {
     enReal.push(word);
   }
 
-  let result1 = numResTxt;
-  let result2 = numResTxt;
+  let result = numResTxt;
   for (let words of enReal) {
     try {
       let data1 = await ahaFunc(words);
-      // let data2 = await papagoFunc(words);
-      result1 = result1.replace(words, `(${words})/(${data1})`);
-      // if (data2 !== "fail") {
-      //   result2 = result2.replace(words, `(${words})/(${data2})`);
-      // } else {
-      //   result2 = "웹 크롤링 실패...다시 시도해주세요!";
-      // }
+      result = result.replace(words, `(${words})/(${data1})`);
     } catch (err) {
       console.error(err);
     }
   }
-  const res = JSON.stringify([result1, result2]);
+  const res = JSON.stringify(result);
   return res;
 });
